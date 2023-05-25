f9f234ada532ce7228fdbba84901a9f540a550d5
Rambabu Rayudu <rayudu07@gmail.com>
2023-05-25 23:11:50 +0530

    files added

---
+++ src/example/GitdemoClass.java
@@ -0,0 +1,13 @@
+package example;
+
+public class GitdemoClass {
+	
+	
+	public static void main(String []array) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Main method");
+
+	}
+
+}
