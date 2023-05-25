a9d274784cef6a06eacf21883236e366acd910a7
Rambabu Rayudu <rayudu07@gmail.com>
2023-05-25 23:39:07 +0530

    added constant file

--- src/example/GitdemoClass.java
+++ src/example/GitdemoClass.java
@@ -6,7 +6,7 @@ public class GitdemoClass {
 	public static void main(String []array) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Main method");
+		System.out.println(Constants.MAIN_METHOD);
 
 	}
 
